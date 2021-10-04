fee4eddf7b0d2d4cfb8a99bbf3e955f146a4e1ba
fiosanabria <87140079+fiosanabria@users.noreply.github.com>
2021-10-04 13:30:38 -0300

    nuevo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,12 @@
 import "./styles/Main.css";
+import Columna from "./Columna.jsx";
+
 const Main = () => {
-  return <main></main>;
+  return <main>
+   <Columna/>
+   <Columna/>
+   
+  </main>;
 };
 
 export default Main;
